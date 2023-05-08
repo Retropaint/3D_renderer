2cdfa3f6efadc89001cc863c1ef466236e2a4027
Retropaint <darkglasses1122@gmail.com>
2023-05-08 21:45:04 +0800

    removed unused func in triangleClipper header

--- triangleClipper/triangleClipper.hpp
+++ triangleClipper/triangleClipper.hpp
@@ -5,5 +5,4 @@ using namespace sf;
 
 bool isFullyCulled(Vector3f* verts);
 void clipTriangle(Triangle mainTri, Triangle *tris, int *tLen);
-int nearClip(Vector3f* verts, Vector3f* points);
 bool isFullyInView(Vector3f verts[3]);
