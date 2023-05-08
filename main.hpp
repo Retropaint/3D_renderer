4526203e7610b1626b24da998de4b7d01d67a98b
Retropaint <darkglasses1122@gmail.com>
2023-05-08 21:42:59 +0800

    emptied main header

--- main.hpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include "structs.hpp"
-
-using namespace sf;
-using namespace std;
-
-void renderLine(Vector3f p1, Vector3f p2, Color color, bool world);
-void playerMove(Vector3f *pos, float angle, float speed);
-void initTriangle(Triangle *tri, Vector3f triTemplate[3]);
