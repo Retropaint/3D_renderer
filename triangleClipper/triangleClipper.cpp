aa46f36ca1c1750d5bf898bd8fd58730b9c02734
Retropaint <darkglasses1122@gmail.com>
2023-08-04 01:26:51 +0800

    added texel calculations to tri clipping

--- triangleClipper/triangleClipper.cpp
+++ triangleClipper/triangleClipper.cpp
@@ -21,9 +21,8 @@ float planeNormalDist(Vector3f point, Vector3f planeNormal, float offset) {
     return planeNormal.x * point.x + planeNormal.y * point.y + planeNormal.z * point.z;
 }
 
-Vector3f linePlaneIntersection(Vector3f p1, Vector3f p2, Vector3f planeNormal, float offset) {
-    float interp = (-offset - (planeNormalDist(p1, planeNormal, 0))) / planeNormalDist(p2 - p1, planeNormal, 0);
-    return p1 + (p2 - p1) * interp;
+float lineToPlaneIntersectInterp(Vector3f p1, Vector3f p2, Vector3f planeNormal, float offset) {
+    return (-offset - (planeNormalDist(p1, planeNormal, 0))) / planeNormalDist(p2 - p1, planeNormal, 0);
 }
 
 bool isFullyCulled(Vector3f* verts) {
@@ -60,32 +59,39 @@ bool isFullyInView(Vector3f verts[3]) {
     return true;
 }
 
-int clip(Vector3f* verts, Vector3f* points, Vector3f planeNormal, int offset) {
+int clip(Triangle tri, Vector3f* points, Vector2f *texels, Vector3f planeNormal, int offset) {
     int pointLen = 0;
     int inVertLen = 0;
     int outVertLen = 0;
-    Vector3f inVerts[3];
-    Vector3f outVerts[3];
-
+    int ivIdx[3];
+    int ovIdx[3];
+    
     // identify which verts are in or out of plane
     for(int i = 0; i < 3; i++) {
-        float distance = planeNormalDist(verts[i], planeNormal, offset);
+        float distance = planeNormalDist(tri.verts[i], planeNormal, offset);
 
         if(distance > 0) {
-            points[pointLen++] = verts[i];
-            inVerts[inVertLen++] = verts[i];
+            points[pointLen] = tri.verts[i];
+            texels[pointLen++] = tri.texels[i];
+            ivIdx[inVertLen++] = i;
         } else {
-            outVerts[outVertLen++] = verts[i];
+            ovIdx[outVertLen++] = i;
         }
     }
 
-    // get points of intersections from in to out verts
+    // get points and texels of intersections from in to out verts
     for(int i = 0; i < inVertLen; i++) {
         for(int j = 0; j < outVertLen; j++) {
             if(offset == near) {
                 offset = -near;
             }
-            points[pointLen++] = linePlaneIntersection(inVerts[i], outVerts[j], planeNormal, offset);
+            float interp = lineToPlaneIntersectInterp(tri.verts[ivIdx[i]], tri.verts[ovIdx[j]], planeNormal, offset);
+
+            Vector3f iv = tri.verts[ivIdx[i]];
+            points[pointLen] = iv + (tri.verts[ovIdx[j]] - iv) * interp;
+            
+            Vector2f it = tri.texels[ivIdx[i]];
+            texels[pointLen++] = it + (tri.texels[ovIdx[j]] - it) * interp;
         }
     }
 
@@ -98,26 +104,29 @@ void clipTriangle(Triangle mainTri, Triangle *tris, int *tLen) {
 
     int planeLen = 6;
     Vector3f planes[planeLen] = { planeNear, planeFar, planeLeft, planeRight, planeTop, planeBottom };
-    int offset[planeLen] = { near, far, 0, 0, 0, 0 }; 
+    float offset[planeLen] = { near, far, 0, 0, 0, 0 };
 
     for(int p = 0; p < planeLen; p++) {
         for(int t = 0; t < *tLen; t++) {
             Triangle tri = tris[t];
 
             Vector3f points[4];
-            int pointLen = clip(tri.verts, points, planes[p], offset[p]);
+            Vector2f texels[4];
+            int pointLen = clip(tri, points, texels, planes[p], offset[p]);
             
             Triangle newTri;
-            newTri.verts[0] = points[0];
-            newTri.verts[1] = points[1];
-            newTri.verts[2] = points[2];
+            for(int i = 0; i < 3; i++) {
+                newTri.verts[i] = points[i];
+                newTri.texels[i] = texels[i];
+            }
             tris[t] = newTri;
 
             if(pointLen == 4) {
                 Triangle newTri2;
-                newTri2.verts[0] = points[1];
-                newTri2.verts[1] = points[2];
-                newTri2.verts[2] = points[3];
+                for(int i = 0; i < 3; i++) {
+                    newTri2.verts[i] = points[i+1];
+                    newTri2.texels[i] = texels[i+1];
+                }
                 tris[*tLen] = newTri2;
                 (*tLen)++;
                 
