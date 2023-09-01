843b0d744c03c3555d53792ba51c13732993ec45
Retropaint <darkglasses1122@gmail.com>
2023-09-01 23:14:19 +0800

    added based hwa lib

---
+++ hwacp/hwaCrossProducts.cu
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+#include <SFML/Graphics.hpp>
+#include <cuda_profiler_api.h>
+#include "../structs.hpp"
+#include "../directives.hpp"
+
+using namespace sf;
+
+__global__
+void triCrossProduct() {
+}
+
+float run() {
+  return 0.0f;
+}
+
+void hwacpInit() {
+}
---
+++ hwacp/hwaCrossProducts.hpp
@@ -0,0 +1,4 @@
+#include <SFML/Graphics.hpp>
+
+float run();
+void hwacpInit();
