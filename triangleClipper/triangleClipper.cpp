7374237ce235ecd526cce9dbce42d186f739b1eb
Osman Mammedov <darkglasses1122@gmail.com>
2024-09-10 23:01:42 +0800

    replaced variable length with constant for clip planes

--- triangleClipper/triangleClipper.cpp
+++ triangleClipper/triangleClipper.cpp
@@ -102,9 +102,8 @@ void clipTriangle(Triangle mainTri, Triangle *tris, int *tLen) {
     tris[0] = mainTri;
     *tLen = 1;
 
-    int planeLen = 6;
-    Vector3f planes[planeLen] = { planeNear, planeFar, planeLeft, planeRight, planeTop, planeBottom };
-    float offset[planeLen] = { near, far, 0, 0, 0, 0 };
+    Vector3f planes[6] = { planeNear, planeFar, planeLeft, planeRight, planeTop, planeBottom };
+    float offset[6] = { near, far, 0, 0, 0, 0 };
 
     for(int p = 0; p < planeLen; p++) {
         for(int t = 0; t < *tLen; t++) {
